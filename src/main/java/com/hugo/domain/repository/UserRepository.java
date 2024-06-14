ad82a959cab14c90234ccad6105377e2cd95b134
huguitorojas <huguitorojas@gmail.com>
2024-06-14 00:23:57 -0300

    model and repository created

---
+++ src/main/java/com/hugo/domain/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.hugo.domain.repository;
+
+import com.hugo.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
